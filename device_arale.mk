cf8ff54ab56a3c1a60fd7e332693554f7eaaaad8
ccfxny <9886712@126.com>
2016-03-23 14:21:00 +0800

    bringup carbon

--- device_arale.mk
+++ device_arale.mk
@@ -4,5 +4,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_arale
 PRODUCT_DEVICE := arale
 
+PRODUCT_COPY_FILES += \
+    device/meizu/mx4/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
 # vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
