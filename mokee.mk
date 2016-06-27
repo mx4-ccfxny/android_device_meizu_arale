3b48dac433120129f67c197f0fd2f97a0342e9a5
ccfxny <9886712@126.com>
2016-06-27 11:26:36 +0800

    bringup mkm

--- mokee.mk
+++ mokee.mk
@@ -4,14 +4,14 @@
 PRODUCT_RELEASE_NAME := MX4
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/meizu/arale/device_arale.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := arale
-PRODUCT_NAME := cm_arale
+PRODUCT_NAME := mk_arale
 PRODUCT_BRAND := Meizu
 PRODUCT_MODEL := MX4
 PRODUCT_MANUFACTURER := Meizu
